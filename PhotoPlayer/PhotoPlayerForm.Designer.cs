b8586e63da55e5c703464a3819ea04b3077a7fea
Lukasz Zaczyk <zaczyk.l@gmail.com>
2021-12-28 12:22:43 +0100

    Add project files.

---
+++ PhotoPlayer/PhotoPlayerForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿
+namespace PhotoPlayer
+{
+    partial class PhotoPlayerForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonLoadPicture = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.buttonRemovePhoto = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // buttonLoadPicture
+            // 
+            this.buttonLoadPicture.Location = new System.Drawing.Point(12, 12);
+            this.buttonLoadPicture.Name = "buttonLoadPicture";
+            this.buttonLoadPicture.Size = new System.Drawing.Size(181, 23);
+            this.buttonLoadPicture.TabIndex = 0;
+            this.buttonLoadPicture.Text = "Wczytaj zdjęcie";
+            this.buttonLoadPicture.UseVisualStyleBackColor = true;
+            this.buttonLoadPicture.Click += new System.EventHandler(this.buttonLoadPicture_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 42);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(181, 76);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 1;
+            this.pictureBox1.TabStop = false;
+            // 
+            // buttonRemovePhoto
+            // 
+            this.buttonRemovePhoto.Location = new System.Drawing.Point(12, 12);
+            this.buttonRemovePhoto.Name = "buttonRemovePhoto";
+            this.buttonRemovePhoto.Size = new System.Drawing.Size(181, 23);
+            this.buttonRemovePhoto.TabIndex = 2;
+            this.buttonRemovePhoto.Text = "Usuń zdjęcie";
+            this.buttonRemovePhoto.UseVisualStyleBackColor = true;
+            this.buttonRemovePhoto.Visible = false;
+            this.buttonRemovePhoto.Click += new System.EventHandler(this.buttonRemovePhoto_Click);
+            // 
+            // PhotoPlayerForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(205, 122);
+            this.Controls.Add(this.buttonRemovePhoto);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.buttonLoadPicture);
+            this.Name = "PhotoPlayerForm";
+            this.Text = "Photo Player";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.PhotoPlayerForm_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonLoadPicture;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button buttonRemovePhoto;
+    }
+}
+
---
+++ PhotoPlayer/PhotoPlayerForm.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security;
+using System.Windows.Forms;
+
+
+namespace PhotoPlayer
+{
+    public partial class PhotoPlayerForm : Form
+    {
+        private string _picturePath;
+        private string _savePath = Path.Combine(Environment.CurrentDirectory, "save.txt");
+        private Size defaultWindowSize = new Size(221, 84);
+        public PhotoPlayerForm()
+        {
+            InitializeComponent();
+            LoadSettings();
+            TryToLoadPicture();
+        }
+        private void LoadSettings()
+        {
+            Size = defaultWindowSize;
+            openFileDialog1.FileName = "Please select an image file";
+            openFileDialog1.Filter = "Image Files(*.BMP; *.JPG; *.GIF)| *.BMP; *.JPG; *.GIF | All files(*.*) | *.*";
+            openFileDialog1.Title = "Open an image file";
+        }
+        private void TryToLoadPicture()
+        {
+            if (File.Exists(_savePath))
+            {
+                _picturePath = File.ReadAllText(_savePath);
+                if (_picturePath.Length!=0)
+                    LoadPictureFromFile();
+            }
+        }
+        private void LoadPictureFromFile()
+        {
+            try
+            {
+                pictureBox1.Image = Image.FromFile(_picturePath);
+                Size = pictureBox1.Image.Size;
+                buttonLoadPicture.Visible = false;
+                buttonRemovePhoto.Visible = true;
+
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show("Niepoprawny typ pliku");
+            }
+        }
+        private void RemovePhoto()
+        {
+            pictureBox1.Image = null;
+            buttonRemovePhoto.Visible = false;
+            buttonLoadPicture.Visible = true;
+            Size = defaultWindowSize;
+        }
+
+
+        //jeśli użytkownik wybierze plik to:
+        //przypisz wybraną przez użytkownika ścieżkę do zmiennej_picturePath
+        //wywołaj metodę LoadPictureFromFile
+        private void buttonLoadPicture_Click(object sender, EventArgs e)
+        {
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                _picturePath = openFileDialog1.FileName;
+                LoadPictureFromFile();
+            }
+        }
+
+        private void buttonRemovePhoto_Click(object sender, EventArgs e)
+        {
+            RemovePhoto();
+        }
+
+        private void PhotoPlayerForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            SavePathToFile();
+        }
+        private void SavePathToFile()
+        {
+            if (pictureBox1.Image != null)
+                File.WriteAllText(_savePath, _picturePath);
+            else
+                File.WriteAllText(_savePath, "");
+        }
+
+    }
+}
